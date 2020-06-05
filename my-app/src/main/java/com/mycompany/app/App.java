01bd4c83242b3d3289fddc17a0f7490cd776361e
naresh-ns <59919934+naresh-ns@users.noreply.github.com>
2020-06-05 07:16:45 +0530

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,13 @@
 package com.mycompany.app;
 
 /**
- * Hello worldns
+ * Hello world
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello worldnst" );
+        System.out.println( "Hello world" );
     }
 }
